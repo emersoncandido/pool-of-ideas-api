4698022aeef508fa7e3480b5968f1aaf7c296e40
Emerson Cozzi Candido <emersoncandido@mec.gov.br>
2023-12-07 11:53:10 -0300

    feat: new tmp endpoint

--- app/main.py
+++ app/main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"placeholder": "Pool of Ideas API"}
+
+@app.get("/workflows")
+def read_root():
+    return {"message": "Testing workflows on GitHub Action"}
