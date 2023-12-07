d722c28356a7dc49aa580301bf00c193069f538f
Emerson Cozzi Candido <emersoncandido@mec.gov.br>
2023-12-07 14:41:56 -0300

    feat: adds tmp endpoint

--- app/main.py
+++ app/main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"placeholder": "Pool of Ideas API"}
+
+@app.get("/workflow")
+def read_workflow():
+    return {"message": "New temporary endpoint"}
