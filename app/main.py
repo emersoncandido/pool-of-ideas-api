27648791dc34ffca17b2e8b1389fd065ac7f045f
Emerson Cozzi Candido <emersoncandido@mec.gov.br>
2023-12-07 14:15:27 -0300

    feat: removes tmp endpoint

--- app/main.py
+++ app/main.py
@@ -6,7 +6,3 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"placeholder": "Pool of Ideas API"}
-
-@app.get("/workflows")
-def read_workflows():
-    return {"message": "Testing workflows on GitHub Action"}
