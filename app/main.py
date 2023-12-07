b49ab704e46247e9703e4d420a080050bf2e1762
Emerson Cozzi Candido <emersoncandido@mec.gov.br>
2023-12-07 14:57:06 -0300

    chore: adds endpoint docstrings

--- app/main.py
+++ app/main.py
@@ -5,8 +5,10 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
+    """Root endpoint"""
     return {"placeholder": "Pool of Ideas API"}
 
 @app.get("/workflow")
 def read_workflow():
+    """Temporary endpoint"""
     return {"message": "New temporary endpoint"}
